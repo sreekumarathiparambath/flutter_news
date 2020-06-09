3de49c15c2bd91d0653b1d00db0ba40f3ea27562
Sreekumar Athiparambath <sreekumaralan@gmail.com>
2020-06-09 11:37:59 +0530

    first commit

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_news/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds return a list of ids', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+    final ids = await newsApi.fetchTopIds();
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('FetchItem returns a ritem item midel', () async {
+    final newsApi = NewsApiProvider();
+    newsApi.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final item = await newsApi.fetchItem(999);
+
+    expect(item.id, 123);
+  });
+}
